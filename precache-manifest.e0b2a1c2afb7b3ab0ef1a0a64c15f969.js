2082a4ea8e9b8de30988c330c12bca1d4499d566
nikol <nikolay.evsevitskiy@gmail.com>
2021-09-24 15:24:11 +0300

    Updates

---
+++ precache-manifest.e0b2a1c2afb7b3ab0ef1a0a64c15f969.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28f5c6311d107a8cae0c6a6b977a59a5",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "d99005516078268e0a6e",
+    "url": "/todolist/static/css/main.db4c93b5.chunk.css"
+  },
+  {
+    "revision": "4e0044aabe0d0c4638a3",
+    "url": "/todolist/static/js/2.f19a5572.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todolist/static/js/2.f19a5572.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d99005516078268e0a6e",
+    "url": "/todolist/static/js/main.2d937062.chunk.js"
+  },
+  {
+    "revision": "471e54e937a3aef18e16",
+    "url": "/todolist/static/js/runtime-main.283d8678.js"
+  }
+]);
