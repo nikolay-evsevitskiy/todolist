eaee9c8c2708744c5ac99bd492aa2481d4b865bd
Nikolay <83282653+nikolay-evsevitskiy@users.noreply.github.com>
2023-04-15 10:45:22 +0200

    the app-reducer is changed on redux toolkit

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -1,50 +1,41 @@
-import {Dispatch} from "redux";
 import {authAPI} from "../api/todolists-api";
 import {setIsLoggedInAC} from "../features/login/auth-reducer";
-import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice, PayloadAction} from "@reduxjs/toolkit";
 
-//state
-const initialState: InitialStateType = {
-    status: "idle",
-    error: null,
-    isInitialized: false
-}
-
+//thunks
+export const initializeAppTC = createAsyncThunk('app/initializeApp', async (param, {dispatch}) => {
+    const res = await authAPI.me()
+    if (res.data.resultCode === 0) {
+        dispatch(setIsLoggedInAC({value: true}))
+    }
+})
+//reducer
 const slice = createSlice({
     name: "app",
-    initialState,
+    initialState: {
+        status: "idle",
+        error: null,
+        isInitialized: false
+    } as InitialStateType,
     reducers: {
         setAppStatusAC(state, action: PayloadAction<{ status: RequestStatusType }>) {
             state.status = action.payload.status
         },
         setAppErrorAC(state, action: PayloadAction<{ error: null | string }>) {
             state.error = action.payload.error
-        },
-        setIsInitializedAC(state, action: PayloadAction<{ isInitialized: boolean }>) {
-            state.isInitialized = action.payload.isInitialized
         }
-
+    },
+    extraReducers: builder => {
+        builder
+            .addCase(initializeAppTC.fulfilled, (state) => {
+                state.isInitialized = true
+            })
     }
 })
 //reducer
 export const appReducer = slice.reducer
 //actions
-export const {setAppStatusAC, setAppErrorAC, setIsInitializedAC} = slice.actions
-
-//thunk
-export const initializeAppTC = () => (dispatch: Dispatch) => {
-    authAPI.me()
-        .then(res => {
-            if (res.data.resultCode === 0) {
-                dispatch(setIsLoggedInAC({value: true}));
-            } else {
-            }
-        })
-        .finally(() => {
-            dispatch(setIsInitializedAC({isInitialized: true}))
-        })
-}
-
+export const {setAppStatusAC, setAppErrorAC} = slice.actions
 //types
 export type InitialStateType = {
     status: RequestStatusType
